fd8301bcd2ed4afb284c65f6980937f31e0c7f71
flatrick-priv <57222317+flatrick-priv@users.noreply.github.com>
2022-06-18 15:08:27 +0200

    less imported namespaces and other improvements. By explicitly declaring each method, there is no question as to where it comes from. As this is meant as an example, the reader shouldn't have to perform any mental juggling to figure out where each method comes from.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Reflection;
-using DbUp;
-using DbUp.Engine;
-using Microsoft.Data.Sqlite;
 
 namespace SQLiteSampleApplication
 {
@@ -11,85 +6,95 @@ namespace SQLiteSampleApplication
     {
         static void Main()
         {
-            SqliteConnection connection = new("Data Source=dbup.db");
-            PermanentSQLiteDb(connection);
-            TemporarySQLiteDb();
-            InMemorySQLiteDb();
+            InMemoryDb();
+            TemporaryFileDb();
+            PermanentFileDb();
         }
 
-        static void PermanentSQLiteDb(SqliteConnection connection)
-        {
-            using (var database = new DbUp.SQLite.Helpers.SharedConnection(connection))
-            {
-                var upgrader = DeployChanges
-                    .To
-                    .SQLiteDatabase(connection.ConnectionString)
-                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
-                    .LogToConsole()
-                    .Build();
-
-                var watch = new Stopwatch();
-                watch.Start();
-
-                var result = upgrader.PerformUpgrade();
-                watch.Stop();
-
-                Display("Permanent file", result, watch.Elapsed);
-            }
-        }
-
-        static void TemporarySQLiteDb()
-        {
-            using (var database = new DbUp.SQLite.Helpers.TemporarySQLiteDatabase("test"))
-            {
-                database.Create();
-
-                var upgrader =
-                    DeployChanges.To
-                    .SQLiteDatabase(database.SharedConnection)
-                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
-                    .LogToConsole()
-                    .Build();
-
-                var watch = new Stopwatch();
-                watch.Start();
-
-                var result = upgrader.PerformUpgrade();
-                watch.Stop();
-
-                Display("Temporary file", result, watch.Elapsed);
-            } // Database will be deleted at this point
-        }
-
-        static void InMemorySQLiteDb()
+        static void InMemoryDb()
         {
             using (var database = new DbUp.SQLite.Helpers.InMemorySQLiteDatabase())
             {
                 var upgrader =
-                    DeployChanges.To
-                    .SQLiteDatabase(database.ConnectionString)
-                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
-                    .LogToConsole()
-                    .Build();
+                    DbUp.DeployChanges.To
+                        .SQLiteDatabase(database.ConnectionString)
+                        .WithScriptsEmbeddedInAssembly(System.Reflection.Assembly.GetExecutingAssembly())
+                        .LogToConsole()
+                        .Build();
 
-                var watch = new Stopwatch();
+                var watch = new System.Diagnostics.Stopwatch();
                 watch.Start();
 
                 var result = upgrader.PerformUpgrade();
                 watch.Stop();
 
                 Display("InMemory", result, watch.Elapsed);
-            } // Database will disappear from memory at this point
+            } // The database will be removed from memory at the end of the using-clause
         }
 
-        static void Display(string dbType, DatabaseUpgradeResult result, TimeSpan ts)
+        static void TemporaryFileDb()
+        {
+            using (var database = new DbUp.SQLite.Helpers.TemporarySQLiteDatabase("test.db"))
+            {
+                var upgrader =
+                    DbUp.DeployChanges.To
+                        .SQLiteDatabase(database.SharedConnection)
+                        .WithScriptsEmbeddedInAssembly(System.Reflection.Assembly.GetExecutingAssembly())
+                        .LogToConsole()
+                        .Build();
+
+                var watch = new System.Diagnostics.Stopwatch();
+                watch.Start();
+
+                var result = upgrader.PerformUpgrade();
+                watch.Stop();
+
+                Display("Temporary file", result, watch.Elapsed);
+
+                /* 
+                Because of a change with dotNet 6.0, all SQLite connections are pooled.
+                This means that there will continue to exist a lock on the file, even after Dispose() has been called when running on Windows.
+                This doesn't occur on Linux because of differences in filesystem operations.
+                A solution is to explicitly tell the SqliteConnection-library to clear all pooled connections before Dispose() is called.
+                https://docs.microsoft.com/en-us/ef/core/what-is-new/ef-core-6.0/breaking-changes#connection-pool
+                */
+                Microsoft.Data.Sqlite.SqliteConnection.ClearAllPools();
+            } // The database-file is deleted at the end of the using-clause
+        }
+
+        static void PermanentFileDb()
+        {
+            Microsoft.Data.Sqlite.SqliteConnection connection = new("Data Source=dbup.db");
+
+            using (var database = new DbUp.SQLite.Helpers.SharedConnection(connection))
+            {
+                var upgrader = DbUp.DeployChanges
+                    .To
+                    .SQLiteDatabase(connection.ConnectionString)
+                    .WithScriptsEmbeddedInAssembly(System.Reflection.Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+                var watch = new System.Diagnostics.Stopwatch();
+                watch.Start();
+
+                DbUp.Engine.DatabaseUpgradeResult result = upgrader.PerformUpgrade();
+                watch.Stop();
+
+                Display("Permanent file", result, watch.Elapsed);
+            } // The file will NOT be removed after the using-clause
+        }
+
+        static void Display(string dbType, DbUp.Engine.DatabaseUpgradeResult result, TimeSpan ts)
         {
             // Display the result
             if (result.Successful)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Success!");
-                Console.WriteLine("{0} Database Upgrade Runtime: {1}", dbType,
+                Console.WriteLine(
+                    "{0} Database Upgrade Runtime: {1}",
+                    dbType,
                     string.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10));
                 Console.ReadKey();
             }

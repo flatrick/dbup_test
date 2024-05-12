bd08b7bd902dbe9462390354b3402a7c7811e9db
flatrick-priv <57222317+flatrick-priv@users.noreply.github.com>
2024-05-12 15:05:30 +0200

    added the use of a config file for the connectionstring in postgres

--- PostgreSQLDb.cs
+++ PostgreSQLDb.cs
@@ -1,89 +1,92 @@
 using System;
 
-namespace dbup_test
+namespace dbup_test;
+
+public class PostgreSQLDb(string connectionString)
 {
-    public static class PostgreSQLDb
+    /// <summary>
+    /// Hold the connection-string provided when creating the instance of this class
+    /// </summary>
+    private readonly string _dbConnectionString = connectionString;
+
+    public void WithScript()
     {
-        public static void WithScript()
-        {
-            const string connectionString = "Host=localhost; Port=2345; Database=mydbc; Username=myuser; Password=mypassword;";
 
-            // Verify that the database exists, if not, create it
-            DbUp.EnsureDatabase.For.PostgresqlDatabase(connectionString);
+        // Verify that the database exists, if not, create it
+        DbUp.EnsureDatabase.For.PostgresqlDatabase(_dbConnectionString);
 
-            /* DropDatabase isn't implemented for PostgreSQL in DbUp */
-            // DbUp.DropDatabase.For.
+        /* DropDatabase isn't implemented for PostgreSQL in DbUp */
+        // DbUp.DropDatabase.For.
 
-            // Configure DbUp.Engine.UpgradeEngine
-            var upgrader = DbUp.DeployChanges.To
-                .PostgresqlDatabase(connectionString)
-                .WithScript("possy_table_init.sql", "CREATE TABLE possy_table (Id INTEGER PRIMARY KEY, Name TEXT NOT NULL);")
-                .LogToConsole()
-                .Build();
+        // Configure DbUp.Engine.UpgradeEngine
+        var upgrader = DbUp.DeployChanges.To
+            .PostgresqlDatabase(_dbConnectionString)
+            .WithScript("possy_table_init.sql", "CREATE TABLE possy_table (Id INTEGER PRIMARY KEY, Name TEXT NOT NULL);")
+            .LogToConsole()
+            .Build();
 
-            var watch = new System.Diagnostics.Stopwatch();
+        var watch = new System.Diagnostics.Stopwatch();
 
-            watch.Start();
-            DbUp.Engine.DatabaseUpgradeResult result = upgrader.PerformUpgrade();
-            watch.Stop();
+        watch.Start();
+        DbUp.Engine.DatabaseUpgradeResult result = upgrader.PerformUpgrade();
+        watch.Stop();
 
-            Program.Display("PostgreSQL - WithScript:", result, watch.Elapsed);
+        Program.Display("PostgreSQL - WithScript:", result, watch.Elapsed);
 
-            /* 
-            https://dbup.readthedocs.io/en/latest/more-info/script-providers/
-            DbUp.DeployChanges.WithScript("name.sql", "content");
-             */
-        }
-        public static void WithScripts()
-        {
-            throw new NotImplementedException();
-            /* DbUp.DeployChanges.WithScripts(new [] {
-                new SqlScript ("name.sql", "content"),
-                new SqlScript ("name.sql", "content")
-                });
-            */
-        }
-
-        public static void WithScriptsCustomScriptProvider()
-        {
-            throw new NotImplementedException();
-            /*
-            builder.WithScripts(new MyCustomScriptProvider());
-            */
-        }
-
-        public static void WithScriptsAndCodeEmbeddedInAssembly()
-        {
-            throw new NotImplementedException();
-            /*
-            builder..WithScriptsAndCodeEmbeddedInAssembly(System.Reflection.Assembly)
-            
-            Make use of run-group-order to showcase that as well.
-            https://github.com/DbUp/DbUp/blob/master/docs/more-info/run-group-order.md
-            */
-        }
-
-        #region Transactions
-
-        /*
-        https://github.com/DbUp/DbUp/blob/master/docs/more-info/transactions.md
-        */
-
-        public static void WithScripts_WithoutTransaction()
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void WithScripts_WithTransactionPerScript()
-        {
-            throw new NotImplementedException();
-        }
-
-        public static void WithScripts_WithTransaction()
-        {
-            throw new NotImplementedException();
-        }
-
-        #endregion Transactions        
+        /* 
+        https://dbup.readthedocs.io/en/latest/more-info/script-providers/
+        DbUp.DeployChanges.WithScript("name.sql", "content");
+         */
     }
+    public void WithScripts()
+    {
+        throw new NotImplementedException();
+        /* DbUp.DeployChanges.WithScripts(new [] {
+            new SqlScript ("name.sql", "content"),
+            new SqlScript ("name.sql", "content")
+            });
+        */
+    }
+
+    public void WithScriptsCustomScriptProvider()
+    {
+        throw new NotImplementedException();
+        /*
+        builder.WithScripts(new MyCustomScriptProvider());
+        */
+    }
+
+    public void WithScriptsAndCodeEmbeddedInAssembly()
+    {
+        throw new NotImplementedException();
+        /*
+        builder..WithScriptsAndCodeEmbeddedInAssembly(System.Reflection.Assembly)
+        
+        Make use of run-group-order to showcase that as well.
+        https://github.com/DbUp/DbUp/blob/master/docs/more-info/run-group-order.md
+        */
+    }
+
+    #region Transactions
+
+    /*
+    https://github.com/DbUp/DbUp/blob/master/docs/more-info/transactions.md
+    */
+
+    public void WithScripts_WithoutTransaction()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void WithScripts_WithTransactionPerScript()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void WithScripts_WithTransaction()
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion Transactions        
 }
--- Program.cs
+++ Program.cs
@@ -1,47 +1,73 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
 
-namespace dbup_test
+namespace dbup_test;
+
+public class Program
 {
-    public static class Program
+    static void Main()
     {
-        static void Main()
+        SQLiteDbExamples();
+        PostgresqlDbExamples(GetPostgresConnectionString());
+    }
+
+    private static void SQLiteDbExamples()
+    {
+        SQLiteDb.InMemorySQLiteDatabase_WithScriptsEmbeddedInAssembly();
+        SQLiteDb.TemporarySQLiteDatabase_WithScriptsEmbeddedInAssemblies();
+        SQLiteDb.SharedConnection_WithScriptsFromFileSystem();
+
+        SQLiteDb.SharedConnection_ScriptType_RunAlways();
+        SQLiteDb.InMemorySQLiteDatabase_ScriptType_RunOnce();
+    }
+
+    private static void PostgresqlDbExamples(string connString)
+    {
+        var postgres = new PostgreSQLDb(connString);
+        postgres.WithScript();
+        postgres.WithScripts();
+        postgres.WithScriptsCustomScriptProvider();
+        postgres.WithScriptsAndCodeEmbeddedInAssembly();
+        postgres.WithScripts_WithoutTransaction();
+        postgres.WithScripts_WithTransactionPerScript();
+        postgres.WithScripts_WithTransaction();
+    }
+
+
+    public static void Display(string dbType, DbUp.Engine.DatabaseUpgradeResult result, TimeSpan ts)
+    {
+        // Display the result
+        if (result.Successful)
         {
-            SQLiteDb.InMemorySQLiteDatabase_WithScriptsEmbeddedInAssembly();
-            SQLiteDb.TemporarySQLiteDatabase_WithScriptsEmbeddedInAssemblies();
-            SQLiteDb.SharedConnection_WithScriptsFromFileSystem();
-
-            SQLiteDb.SharedConnection_ScriptType_RunAlways();
-            //SQLiteDb.InMemorySQLiteDatabase_ScriptType_RunOnce();
-
-            //PostgreSQLDb.WithScript();
-            //PostgreSQLDb.WithScripts();
-            //PostgreSQLDb.WithScriptsCustomScriptProvider();
-            //PostgreSQLDb.WithScriptsAndCodeEmbeddedInAssembly();
-            //PostgreSQLDb.WithScripts_WithoutTransaction();
-            //PostgreSQLDb.WithScripts_WithTransactionPerScript();
-            //PostgreSQLDb.WithScripts_WithTransaction();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.WriteLine(
+                "{0} Database Upgrade Runtime: {1}",
+                dbType,
+                string.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10));
+            Console.ReadKey();
         }
-
-        public static void Display(string dbType, DbUp.Engine.DatabaseUpgradeResult result, TimeSpan ts)
+        else
         {
-            // Display the result
-            if (result.Successful)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Success!");
-                Console.WriteLine(
-                    "{0} Database Upgrade Runtime: {1}",
-                    dbType,
-                    string.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10));
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(result.Error);
-                Console.ReadKey();
-                Console.WriteLine("Failed!");
-            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(result.Error);
+            Console.ReadKey();
+            Console.WriteLine("Failed!");
         }
     }
+    
+    /// <summary>
+    /// Helper-method to deal with reading and returning the connection-string for PostgreSQL in AppSettings.json
+    /// </summary>
+    /// <returns>A connectionstring in the form of a string</returns>
+    private static string GetPostgresConnectionString()
+    {
+        IConfigurationRoot config = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json")
+        .AddEnvironmentVariables()
+        .Build();
+
+        return config.GetConnectionString("postgres")!;
+    }
 }
+
